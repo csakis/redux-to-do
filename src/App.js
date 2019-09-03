b4a6eeaf5fef53d3a195de44200c30a91976e19c
Csaba Kiss <csakis@lanl.gov>
2019-09-03 08:25:28 -0600

    added react and redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStore} from 'redux';
+
+import {Provider} from 'react-redux'
+
+function App() {
+  return (
+
+    <div>
+     
+    </div>
+  );
+}
+
+export default App;
